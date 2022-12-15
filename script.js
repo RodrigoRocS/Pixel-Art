1af607cb0237c9e540d30b13452d3a6bfe954818
RodrigoRocS <rrsilva1701@gmail.com>
2022-12-14 23:21:06 -0300

    requisito 5,6 e 7 concluidos]

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+const pegaBody = document.getElementsByTagName('body')[0];
+
 const titleHeader = () => {
-  const headerH1 = document.getElementsByTagName('body')[0];
   const creatH1 = document.createElement('h1');
   creatH1.id = 'title';
   creatH1.innerText = 'Paleta de Cores';
-  headerH1.appendChild(creatH1);
+  pegaBody.appendChild(creatH1);
 };
 
 const colorPallet = () => {
-  const palletP = document.getElementsByTagName('body')[0];
   const colorPallets = document.createElement('div');
   colorPallets.id = 'color-palette';
-  palletP.appendChild(colorPallets);
+  pegaBody.appendChild(colorPallets);
 };
 
 const colorBoxes = () => {
@@ -19,16 +19,11 @@ const colorBoxes = () => {
     const colorBox = document.createElement('div');
     colorBox.className = 'color';
     colorBox.style.border = '1px solid black';
-    colorBox.style.display = 'inline';
+    colorBox.style.display = 'inline-block';
     colorBox.style.padding = '25px';
     colorPallets.appendChild(colorBox);
   }
 };
-
-// const primeBlack = () => {
-//   const paintBoxCol = document.getElementsByClassName('color');
-//   paintBoxCol[0].style.backgroundColor = 'rgb(0,0,0)';
-// };
 
 const randomCollors = () => {
   const r = Math.floor(Math.random() * 255);
@@ -48,14 +43,73 @@ const paintBox = () => {
   }
 };
 
+const saveFirstPallet = () => {
+  const pegaCores = document.querySelectorAll('.color');
+  const saveColor = [];
+  for (let index = 0; index < pegaCores.length; index += 1) {
+    saveColor.push(pegaCores[index].style.backgroundColor);
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(saveColor));
+};
+
+const paintBox2 = () => {
+  const paintBoxCol = document.getElementsByClassName('color');
+  for (let index = 1; index < paintBoxCol.length; index += 1) {
+    paintBoxCol[index].style.backgroundColor = randomCollors();
+    if (paintBoxCol[0].style.backgroundColor !== 'rgba(0,0,0)') {
+      paintBoxCol[0].style.backgroundColor = 'rgba(0,0,0)';
+    }
+  }
+  saveFirstPallet();
+};
+
 const changeColBtn = () => {
-  const paiDoBtn = document.getElementsByTagName('body')[0];
   const criaBtn = document.createElement('button');
   criaBtn.id = 'button-random-color';
   criaBtn.innerText = 'Cores aleatórias';
-  criaBtn.addEventListener('click', paintBox);
+  criaBtn.addEventListener('click', paintBox2);
   criaBtn.style.marginTop = '25px';
-  paiDoBtn.appendChild(criaBtn);
+  pegaBody.appendChild(criaBtn);
+};
+
+const returnFirstPalett = () => {
+  const pegaCores = document.querySelectorAll('.color');
+  const getColorsBack = JSON.parse(localStorage.getItem('colorPalette'));
+
+  for (let index = 0; index < getColorsBack.length; index += 1) {
+    pegaCores[index].style.backgroundColor = getColorsBack[index];
+  }
+};
+
+const criarPainel = () => {
+  const painelCriar = document.createElement('div');
+  painelCriar.id = 'pixel-board';
+  pegaBody.appendChild(painelCriar);
+  painelCriar.style.display = 'inline-block';
+  painelCriar.style.padding = '25px';
+  painelCriar.style.border = '2px solid black';
+
+  for (let index = 0; index < 5; index += 1) {
+    const criaLinha = document.createElement('div');
+    // criaLinha.className = 'pixel';
+    painelCriar.appendChild(criaLinha);
+    for (let index2 = 0; index2 < 5; index2 += 1) {
+      const criaColuna = document.createElement('div');
+      criaColuna.className = 'pixel';
+      criaLinha.appendChild(criaColuna);
+    }
+  }
+};
+
+const pixelCss = () => {
+  const pegaPixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pegaPixel.length; index += 1) {
+    pegaPixel[index].style.backgroundColor = 'white';
+    pegaPixel[index].style.width = '40px';
+    pegaPixel[index].style.height = '40px';
+    pegaPixel[index].style.border = '1px solid black';
+    pegaPixel[index].style.display = 'inline-block';
+  }
 };
 
 window.onload = () => {
@@ -63,5 +117,10 @@ window.onload = () => {
   colorPallet();
   colorBoxes();
   paintBox();
+  criarPainel();
+  pixelCss();
   changeColBtn();
+  if (localStorage.getItem('colorPalette')) {
+    returnFirstPalett();
+  }
 };
